b947de5e0ac8381785a6632dff2a38a3b403ce21
ArchbowMistress <jhonnaelman@gmail.com>
2025-11-06 23:21:04 +0800

    pain

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody rb;
+    [SerializeField] public float movementSpeed = 50f;
+    [SerializeField] public float jumpSpeed = 10f;
+    [SerializeField] public float maxSpeed = 50f;
+    private float gravity = -9.81f;
+    [SerializeField] private float gravityMultiplier = 3.0f;
+    private float velocity;
+    public Vector3 boxSize;
+    public float castDistance;
+    public LayerMask groundLayer;
+
+
+    void FixedUpdate()
+    {
+        PlayerMovement();
+        ManualGravity();
+
+    }
+
+    public bool isGrounded()
+    {
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), castDistance, groundLayer))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position - transform.TransformDirection(Vector3.up) * castDistance, boxSize);
+    }
+
+    void ManualGravity()
+    {
+        if (isGrounded() && velocity < 0.0f)
+        {
+            // Debug.Log("ITS A HIT");
+            velocity = -1.0f;
+            rb.AddForce(0, velocity * Time.deltaTime, 0, ForceMode.VelocityChange);
+        }
+        else
+        {
+            // Debug.Log("nah");
+            velocity += gravity * gravityMultiplier * Time.deltaTime;
+
+            rb.AddForce(0, velocity * Time.deltaTime, 0, ForceMode.VelocityChange);
+        }
+    }
+
+    void PlayerMovement()
+    {
+        //Max Speed Cap
+        //Debug.Log(rb.velocity.magnitude);
+        if (rb.velocity.magnitude > maxSpeed)
+        {
+            rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
+
+        }
+        
+
+
+
+        if (Input.GetKey("w"))
+        {
+            // Debug.Log("Forward");
+            rb.AddForce(0, 0, movementSpeed * Time.deltaTime, ForceMode.VelocityChange);
+        }
+        //Right
+        if (Input.GetKey("s"))
+        {
+            // Debug.Log("Backward");
+            rb.AddForce(0, 0, -movementSpeed * Time.deltaTime, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a"))
+        {
+            // Debug.Log("Left");
+            rb.AddForce(-movementSpeed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        //Right
+        if (Input.GetKey("d"))
+        {
+            // Debug.Log("Right");
+            rb.AddForce(movementSpeed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        //Jump
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded())
+        {
+            // Debug.Log("Jump");
+            rb.AddForce(0, jumpSpeed * Time.deltaTime, 0, ForceMode.VelocityChange);
+        }
+    }
+
+    
+}
---
+++ Assets/Scripts/Tracker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tracker : MonoBehaviour
+{
+    public GameObject startPosition;
+    public float distance;
+    public Text distanceText;
+    public Vector3 currentPosition, initialPosition;
+    
+    void FixedUpdate()
+    {
+        TrackDistance();
+    }
+    public string TrackDistance()
+    {
+        distance = Vector3.Distance(startPosition.transform.position, transform.position);
+        currentPosition = transform.position;
+        initialPosition = startPosition.transform.position;
+
+        distanceText.text = "Distance from Spawn: " + distance.ToString();
+        return distanceText.text;
+        
+    }
+
+}
